42cae5e40e608452d924d2d4466c16dc80e1c77a
Steven Sanchez <Stevensimpson05@gmail.com>
2020-08-04 21:23:17 -0500

    Project kick off

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Project to obtain the quotes of the most important cryptocurrencies
+      through the Coincap REST API.
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Return to homepage</router-link
+    >
+  </div>
+</template>
