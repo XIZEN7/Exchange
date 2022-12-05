cc27035ff26c16795278303d324ddcd16b1f6631
Steven Sanchez <stevensimpson05@gmail.com>
2022-12-04 23:18:16 -0500

    changes in index html

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -2,7 +2,6 @@
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl uppercase">error</h1>
     <router-link class="mt-5 text-xl text-green-600 hover:underline" to="/"
-      >Return to homepage</router-link
-    >
+      >Return to homepage</router-link>
   </div>
 </template>
